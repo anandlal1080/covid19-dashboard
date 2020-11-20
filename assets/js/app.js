dd74019d229714a18cd49fe1c6b8b45617344507
Tucker <tuckerbeauchamp@gmail.com>
2020-11-20 11:46:52 -0700

    render country options

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,75 @@
-$(document).foundation()
+$(document).foundation();
 
+const states_array = [
+  "AK",
+  "AL",
+  "AR",
+  "AZ",
+  "CA",
+  "CO",
+  "CT",
+  "DC",
+  "DE",
+  "FL",
+  "GA",
+  "HI",
+  "IA",
+  "ID",
+  "IL",
+  "IN",
+  "KS",
+  "KY",
+  "LA",
+  "MA",
+  "MD",
+  "ME",
+  "MI",
+  "MN",
+  "MO",
+  "MS",
+  "MT",
+  "NC",
+  "ND",
+  "NE",
+  "NH",
+  "NJ",
+  "NM",
+  "NV",
+  "NY",
+  "OH",
+  "OK",
+  "OR",
+  "PA",
+  "PR",
+  "RI",
+  "SC",
+  "SD",
+  "TN",
+  "TX",
+  "UT",
+  "VA",
+  "VI",
+  "VT",
+  "WA",
+  "WI",
+  "WV",
+  "WY",
+];
 
-
-let states = new Array();
-const states_array = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VI', 'VT', 'WA', 'WI', 'WV', 'WY'];
-
-$.each(states_array, function(val, text) {
-    $('#states').append( $('<option></option>').val(val).html(text) )
+fetch("https://api.covid19api.com/countries")
+  .then((data) => data.json())
+  .then(function (countries) {
+    countries.forEach(function ({ Slug, Country }) {
+      $("#countries").append($("<option></option>").val(Slug).html(Country));
     });
+  });
 
-    let countries = new Array();
-    const ww_array = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VI', 'VT', 'WA', 'WI', 'WV', 'WY'];
-    
-    $.each(ww_array, function(val, text) {
-        $('#countries').append( $('<option></option>').val(val).html(text) )
-        }); 
+// fetch("https://api.covidtracking.com/v1/states/")
+//   .then((data) => data.json())
+//   .then(function (states) {
+//     console.log(states);
+//   });
+
+states_array.forEach(function (state) {
+  // $("#states").append($("<option></option>").val(val).html(text));
+});
